094d08ce7a6b7771073d04950045c4d8d620ddd1
Saeid Farivar Asanjan <farivar@google.com>
2021-10-29 15:25:24 -0700

    Fix referencing a AdapterV1 class from app classloader
    *AdapterV1 classes should only be accessed from classes that are loaded
    from the plugin classloader.
    
    Bug: 194095014
    Test: atest CarUILibUnitTests
    Change-Id: I48fcaa32a33a4ffb304593c964bc48787b248e29

--- car-ui-lib/car-ui-lib/src/main/java/com/android/car/ui/recyclerview/CarUiRecyclerViewImpl.java
+++ car-ui-lib/car-ui-lib/src/main/java/com/android/car/ui/recyclerview/CarUiRecyclerViewImpl.java
@@ -55,6 +55,7 @@ import androidx.recyclerview.widget.RecyclerView.OnFlingListener;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 
 import com.android.car.ui.R;
+import com.android.car.ui.preference.PreferenceFragment.AndroidxRecyclerViewProvider;
 import com.android.car.ui.recyclerview.decorations.grid.GridDividerItemDecoration;
 import com.android.car.ui.recyclerview.decorations.linear.LinearDividerItemDecoration;
 import com.android.car.ui.utils.CarUxRestrictionsUtil;
@@ -75,7 +76,7 @@ import java.util.Set;
 @SuppressLint("CustomViewStyleable")
 @RequiresApi(MIN_TARGET_API)
 public final class CarUiRecyclerViewImpl extends FrameLayout
-        implements CarUiRecyclerView, LazyLayoutView {
+        implements CarUiRecyclerView, LazyLayoutView, AndroidxRecyclerViewProvider {
 
     private static final String TAG = "CarUiRecyclerView";
     /**
@@ -379,6 +380,7 @@ public final class CarUiRecyclerViewImpl extends FrameLayout
         return mRecyclerView.getAdapter();
     }
 
+    @Override
     public RecyclerView getRecyclerView() {
         return mRecyclerView;
     }
