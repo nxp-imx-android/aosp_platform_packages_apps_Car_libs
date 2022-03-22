eb77f388c1c8d5dcabd92205572b90603a204fd4
Ram Parameswaran <rampara@google.com>
2022-03-22 07:10:24 +0000

    Update Paintbooth widget activity to include more widgets
    Fixes: 226009227
    Test: Manual
    
    Change-Id: I552bee994ab2a8e5eee43383ce060582557cd53b

--- car-ui-lib/paintbooth/src/main/java/com/android/car/ui/paintbooth/widgets/WidgetActivity.java
+++ car-ui-lib/paintbooth/src/main/java/com/android/car/ui/paintbooth/widgets/WidgetActivity.java
@@ -18,23 +18,49 @@ package com.android.car.ui.paintbooth.widgets;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.android.car.ui.core.CarUi;
 import com.android.car.ui.paintbooth.R;
+import com.android.car.ui.toolbar.MenuItem;
 import com.android.car.ui.toolbar.NavButtonMode;
 import com.android.car.ui.toolbar.ToolbarController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Activity that shows different widgets from the device default theme.
  */
 public class WidgetActivity extends Activity {
+    private boolean mIsEnabled = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.widgets_activity);
+
+        List<View> widgets = new ArrayList<>();
+        widgets.add(requireViewById(R.id.check));
+        widgets.add(requireViewById(R.id.switch_widget));
+        widgets.add(requireViewById(R.id.toggle_button));
+        widgets.add(requireViewById(R.id.radio_button));
+        widgets.add(requireViewById(R.id.seekbar));
+
         ToolbarController toolbar = CarUi.requireToolbar(this);
         toolbar.setTitle(getTitle());
         toolbar.setNavButtonMode(NavButtonMode.BACK);
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(MenuItem.builder(this)
+                .setTitle(getString(R.string.widget_enable_button_text))
+                .setOnClickListener(
+                        i -> {
+                            mIsEnabled = !mIsEnabled;
+                            for (View view : widgets) {
+                                view.setEnabled(mIsEnabled);
+                            }
+                        })
+                .build());
+        toolbar.setMenuItems(menuItems);
     }
 }
